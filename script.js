4660ed6207301a2037e692c94599ff4bff55f68d
naomidzunic <wildandfreeservices@gmail.com>
2022-07-18 12:32:08 -0500

    finish styling homepage basics with html & css

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const toggleButton = document.getElementById('toggle-button')[0]
+const hamburgerIcon = document.getElementsByClassName('hamburger-icon')[0]
 const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
-toggleButton.addEventListener('click' , () => {
+hamburgerIcon.addEventListener('click', () => {
     navbarLinks.classList.toggle('active')
 })
