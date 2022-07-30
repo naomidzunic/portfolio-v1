7ea244532ed3c8bd0f1e71845fabfd947ee62fb9
naomidzunic <wildandfreeservices@gmail.com>
2022-07-30 11:21:29 -0500

    updating changes on desktop

--- script.js
+++
@@ -1,6 +0,0 @@
-const hamburgerIcon = document.getElementsByClassName('hamburger-icon')[0]
-const navbarLinks = document.getElementsByClassName('navbar-links')[0]
-
-hamburgerIcon.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
-})
